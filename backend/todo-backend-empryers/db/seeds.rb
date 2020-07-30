f29b56884a1bb6c8c9203080541e1a887b52da8c
github username <github email address>
2020-07-29 20:02:04 -0600

    Finished  Todo model, Routes, and Seeds data

--- backend/todo-backend-empryers/db/seeds.rb
+++ backend/todo-backend-empryers/db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Todo.destroy_all
+User.destroy_all 
+
+chris = User.create(username: "chris123", password: "chris123")
 
 Todo.create(title: "First Todo", content: "This is my first todo item", urgent: false, done: false)
 Todo.create(title: "Second Todo", content: "This is my second todo item", urgent: false, done: false)
