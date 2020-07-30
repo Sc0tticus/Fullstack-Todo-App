5d0e1f5eb8fbcc02bc7974a700829a5666a71faa
github username <github email address>
2020-07-29 20:34:35 -0600

    Finished Users Controller, User Model, Users Migration, and seeded DataBase

---
+++ backend/todo-backend-empryers/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+    def profile 
+        render json: {user: current_user, todos: current_user.todos}
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.valid? 
+            @user.save 
+            @token = JWT.encode({user_id: @user.id}, "yerrr")
+            render json: {user: @user, todos: @user.todos, token: @token}, status: :created
+        else 
+            render json: {errors: @user.errors.full_messages}, status: :not_acceptable
+        end
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+    
+end
